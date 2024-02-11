3a4283c6417374224f3f8e5587602b36813797d2
thatikantianil <thatikantianil@outlook.com>
2024-02-11 16:58:59 +0530

    board component created

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import { Board } from "./modules/board";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>anil</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Board />} />
+          <Route path="/app" element={<Board />} />
+          {/* <Route exact path="/app" element={<Board />} /> */}
+          <Route path="*" element={<p>Page not found!</p>} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
